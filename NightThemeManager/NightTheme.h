82c52690c60528a5ce3ed441b997321d20dbe2d0
Silence <374619540@qq.com>
2017-07-03 23:48:17 +0800

    Update

---
+++ NightThemeManager/NightTheme.h
@@ -0,0 +1,15 @@
+//
+//  NightTheme.h
+//  NightThemeManagerDemo
+//
+//  Created by 杨晴贺 on 2017/7/3.
+//  Copyright © 2017年 Silence. All rights reserved.
+//
+
+#ifndef NightTheme_h
+#define NightTheme_h
+
+#import "SINightThemeManager.h"
+#import "UIView+NightTheme.h"
+
+#endif /* NightTheme_h */
